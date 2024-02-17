1ce40c30b48677a52c2a62df5c5f487f5de6e544
V-Gnana-Teja <151600407+V-Gnana-Teja@users.noreply.github.com>
2024-02-17 22:28:03 +0530

    Initial Commit

---
+++ apple-landing-page/src/App.js
@@ -0,0 +1,15 @@
+import { Button, Alert } from 'react-native';
+import logo from './logo.svg';
+import './App.css';
+import {Navigation} from './components/navigation'
+
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
